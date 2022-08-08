90d7440e7329d0cc8947a19cc4973dee296e1b51
Kyle Hench <kylehench@gmail.com>
2022-08-08 10:57:30 -0700

    updated cors policy

--- server/server.js
+++ server/server.js
@@ -8,8 +8,9 @@ const app = express()
 
 // cookies in express
 app.use(cookieParser());
+app.use(cors())
 // Change the app.use(cors()) to the one below
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+// app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))   // This allows JSON Objects with strings and arrays
 
