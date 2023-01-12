f6fcad4061e6bc8379a8e8d7b8eec2475ca88852
Kyle Hench <kylehench@gmail.com>
2023-01-11 17:15:21 -0800

    changed BrowserRouter basename to PUBLIC_URL

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.REACT_APP_URL_BASENAME}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
