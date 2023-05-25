368deed0ca89205ddf696ee9b4c4940e2868915e
Kyle Hench <kylehench@gmail.com>
2023-05-24 20:59:13 -0700

    .env var used for URL basename in vite.config

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, node: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import svgr from "vite-plugin-svgr";
+import svgr from "vite-plugin-svgr"
+import dotenv from 'dotenv'
+
+// Load environment variables from .env file
+dotenv.config();
+const { VITE_URL_BASENAME } = process.env
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/mazique',
+  base: VITE_URL_BASENAME,
   server: {
     proxy: {
-      '/mazique/api': {
+      [`${VITE_URL_BASENAME}/api`]: {
         target: 'http://localhost:8000/',
-        rewrite: (path) => path.replace(/^\/mazique/, '')
+        rewrite: (path) => path.replace(new RegExp(VITE_URL_BASENAME), '')
       }
     }
   }
