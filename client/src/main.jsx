f0d1547369cfc72de4b31ee2b02075f390a2ce9c
Kyle Hench <kylehench@gmail.com>
2023-05-24 17:01:47 -0700

    changed "PUBLIC_URL" to "URL_BASENAME"

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.VITE_PUBLIC_URL}>
+    <BrowserRouter basename={import.meta.env.VITE_URL_BASENAME}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
