bf995d91faba63b3dde3ddce425b206beda37ef6
kylehench <94999310+kylehench@users.noreply.github.com>
2024-02-16 11:12:25 -0800

    mongoose.config.js - use ipv4

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -5,6 +5,8 @@ DBName = 'scores'
 // Make `sanitizeFilter` true by default
 mongoose.set('sanitizeFilter', true)
 //This will create a database named DBName if one doesn't already exist (no need for mongo shell!)
-mongoose.connect(`mongodb://localhost/${DBName}`)
+mongoose.connect(`mongodb://localhost/${DBName}`, {
+    family: 4
+})
     .then(() => console.log(`Established a connection to the database '${DBName}'`))
     .catch(err => console.log('Something went wrong when connecting to the database', err))
